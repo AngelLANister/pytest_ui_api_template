417873e0ce5945adb4765267ef91186438622f99
AngelLANister <bezrukandrej2@gmail.com>
2026-03-26 15:40:41 +0700

    Убраны комментарии

--- test/test_ui.py
+++ test/test_ui.py
@@ -6,39 +6,39 @@ from Page.MainPage import MainPage
 from Page.ChancePage import ChancePage
 
 
-# def test_search(browser):
-#     main_page = MainPage(browser)
-#     main_page.clear_page()
-#     film_title = main_page.film("Холоп")
+def test_search(browser):
+    main_page = MainPage(browser)
+    main_page.clear_page()
+    film_title = main_page.film("Холоп")
 
-#     with allure.step("Проверить, что название фильма соответствует запросу"):
-#         assert film_title == "Холоп"
+    with allure.step("Проверить, что название фильма соответствует запросу"):
+        assert film_title == "Холоп"
 
 
-# def test_search_random_film(browser):
-#     chance_page = ChancePage(browser)
-#     rnd_film = chance_page.get_random_film()
-#     main_page = MainPage(browser)
-#     film_title = main_page.film(rnd_film)
+def test_search_random_film(browser):
+    chance_page = ChancePage(browser)
+    rnd_film = chance_page.get_random_film()
+    main_page = MainPage(browser)
+    film_title = main_page.film(rnd_film)
  
-#     with allure.step("Проверить, что случайный фильм соответствует запросу"):
-#         assert film_title == rnd_film
+    with allure.step("Проверить, что случайный фильм соответствует запросу"):
+        assert film_title == rnd_film
 
 
-# def test_reviews(browser):
-#     chance_page = ChancePage(browser)
-#     review_text = chance_page.get_review()
+def test_reviews(browser):
+    chance_page = ChancePage(browser)
+    review_text = chance_page.get_review()
 
-#     with allure.step("Проверить, что открываются рецензии зрителей"):
-#         assert review_text == "Рецензии зрителей"
+    with allure.step("Проверить, что открываются рецензии зрителей"):
+        assert review_text == "Рецензии зрителей"
 
 
-# def test_floating_hints(browser):
-#     chance_page = ChancePage(browser)
-#     hint_text = chance_page.get_hints()
+def test_floating_hints(browser):
+    chance_page = ChancePage(browser)
+    hint_text = chance_page.get_hints()
 
-#     with allure.step("Проверить, что отображаются всплывающие подсказки"):
-#         assert hint_text == "Возможно, вы искали"
+    with allure.step("Проверить, что отображаются всплывающие подсказки"):
+        assert hint_text == "Возможно, вы искали"
 
 
 def test_search_invalid_film(browser):
